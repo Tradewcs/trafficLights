a3f63e81a43e2a60c682bbd13591985d8ec0a58f
Tradewcs <tradewcs@gmail.com>
2024-11-06 21:41:37 +0200

    fix some errors

--- googlescripts.js
+++ googlescripts.js
@@ -1,56 +1,24 @@
-const idDb = '1p0GMbeNKIQcDUv516MN5Y02XbZpWoaIG-FErgmBPe5Y';
+const idDb = '1FISnspg_SY7CSznP_N3GOsQbL3sTWX84F1er34r9wo4';
 const ss = SpreadsheetApp.openById(idDb);
 const sheet = ss.getSheetByName('Sheet1');
 
-function initializeSheet() {
-  if (sheet.getLastRow() === 0) {
-    sheet.appendRow(['ID', 'State']);
-    sheet.appendRow([1, 'red']);
-    sheet.appendRow([2, 'green']);
-  }
+function doPost(e) {
+  var state = e.parameter.state;
+  var isAutomatic = e.parameter.isAutomatic;
+  var timestamp = new Date().toISOString();
+
+  var lastRow = sheet.getLastRow();
+  
+  var lastStateChangeCount = lastRow > 0 ? sheet.getRange(lastRow, 4).getValue() : 0;
+  
+  var stateChangeCount = lastStateChangeCount + 1;
+  sheet.appendRow([state, timestamp, isAutomatic, stateChangeCount]);
+
+  return ContentService.createTextOutput('State updated successfully');
 }
 
-function getTrafficLights() {
-  const data = sheet.getDataRange().getValues();
-  const lights = data.slice(1).map(row => ({ id: row[0], state: row[1] }));
-  return lights;
-}
-
-function getTrafficLightState(id) {
-  const data = getTrafficLights();
-  const light = data.find(light => light.id === id);
-  return light || { error: 'Traffic light not found' };
-}
-
-function updateTrafficLightState(id, state) {
-  const data = sheet.getDataRange().getValues();
-  const rowIndex = data.findIndex(row => row[0] === id);
-
-  if (rowIndex > -1) {
-    sheet.getRange(rowIndex + 1, 2).setValue(state);
-    return getTrafficLights();
-  } else {
-    return { error: 'Traffic light not found' };
-  }
-}
-
-function doGet(request) {
-  initializeSheet();
-  const { method, id, state } = request.parameter;
-
-  if (method === 'GET_ALL') {
-    return sendJson(getTrafficLights());
-  }
-  if (method === 'GET' && id) {
-    return sendJson(getTrafficLightState(Number(id)));
-  }
-  if (method === 'POST' && id && state) {
-    return sendJson(updateTrafficLightState(Number(id), state));
-  }
-
-  return sendJson({ error: 'Invalid request' });
-}
-
-function sendJson(response) {
-  return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(ContentService.MimeType.JSON);
+function doGet(e) {
+  var data = sheet.getDataRange().getValues();
+  var jsonResponse = JSON.stringify(data);
+  return ContentService.createTextOutput(jsonResponse).setMimeType(ContentService.MimeType.JSON);
 }
